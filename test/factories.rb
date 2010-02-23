9a19821c046ea3c55064f310fccb95502154982d
Ivan Schneider - ivan <isc@massivebraingames.com>
2010-02-23 17:22:26 +0100

    forgot to add the factories

---
+++ test/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |u|
+  u.openid_identifier 'https://openid.octo.com/users/fhi'
+  u.username 'fhisquin'
+  u.lastname 'Hisquin'
+  u.firstname 'François'
+  u.twitter_identifier 'fhisquin'
+end
