5108f65e71d7a26fcafcd0e304d4e136929dfd43
Ivan Schneider - ivan <isc@massivebraingames.com>
2010-02-23 17:47:40 +0100

    .gems updated

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,7 @@ config.gem "rspec", :lib => false, :version => ">=1.2.2"
 config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
 config.gem "webrat", :lib => false, :version => ">=0.4.3"
 config.gem "cucumber", :lib => false, :version => ">=0.2.2"
+config.gem 'cucumber-rails', :lib => false
 config.gem "mocha", :version => ">=0.9.8"
 config.gem "fakeweb", :lib => false
 config.gem 'factory_girl'
