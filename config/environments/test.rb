2573a51af753c76e8fd8427ef307093aa3d61762
Christian Blavier <cblavier@octo.com>
2010-02-24 12:07:19 +0100

    Test avec suppression des accents

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,7 +30,7 @@ config.action_mailer.delivery_method = :test
 config.gem "rspec", :lib => false, :version => ">=1.2.2"
 config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
 config.gem "webrat", :lib => false, :version => ">=0.4.3"
-config.gem "cucumber", :lib => false, :version => ">=0.2.2"
+config.gem "cucumber", :lib => false, :version => ">=0.6.2"
 config.gem 'cucumber-rails', :lib => false
 config.gem "mocha", :version => ">=0.9.8"
 config.gem "fakeweb"
