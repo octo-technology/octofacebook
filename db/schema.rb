c5f868462fc99e2752c512cf1b432c49bd983c83
Christian Blavier <cblavier@octo.com>
2010-02-24 10:38:32 +0100

    Ajout d'un h3 pour les posts octotalks

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100223194531) do
+ActiveRecord::Schema.define(:version => 20100223233710) do
 
   create_table "open_id_authentication_associations", :force => true do |t|
     t.integer "issued"
