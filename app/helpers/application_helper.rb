ce9a558b87e22c4a2daf3026582358f03c7465b4
Christian Blavier <cblavier@octo.com>
2010-02-25 23:29:11 +0100

    gravatar is now a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def gravatar(user)
+    image_tag( "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.username + "@octo.com")}")
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,4 @@
-module AccountsHelper
+module UsersHelper
   
  def twitter_identifier_url (twitter_identifier) 
    "http://www.twitter.com/#{twitter_identifier}"
